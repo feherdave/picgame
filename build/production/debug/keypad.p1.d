14705310c99270d6bb1a90554677da39c1a29992
feherdave <feherdave@gmail.com>
2023-11-06 23:58:30 +0100

    Fine tuned randomizer and modified truck game difficulty & speed values

--- build/production/debug/keypad.p1.d
+++
@@ -1,5 +0,0 @@
- build/production/debug/keypad.d  \
- build/production/debug/keypad.p1:  \
- keypad.c  \
-C:/Program\ Files/Microchip/MPLABX/v6.15/packs/Microchip/PIC16Fxxx_DFP/1.4.149/xc8/pic/include/language_support.h  \
-keypad.h 
